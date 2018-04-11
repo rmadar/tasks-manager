b3acd91d5cf52db29cbc4d20b1186e6eeea00a8e
Romain Madar <romain.madar@cern.ch>
2018-04-11 17:39:28 +0200

    Enable reading data from configuration files

--- analysis_management.py
+++ analysis_management.py
@@ -4,31 +4,19 @@
 ##-------------------------------------------------
 class Study:
 
-    def __init__(self,date,contributor,link,**kwargs):
+    def __init__(self,date,contributor,link,title='Update'):
         """
         Study() class is the element describing a given study and its documentation:
          - date: date of the presentation (string)
          - contributor: speaker (string)
          - link: indico link (string)
-         - keyword arguments can take the following:
-           * meeting: string describing the type of meeting (e.g. weekly meeting, workshop)
-           * title: srting describing the title of the contribution (e.g. 'Final result')
+         - title: contribution title (string)
         """
         self.date=date
         self.contributor=contributor
         self.link=link
-
-        try:
-            self.meeting = kwargs['meeting']
-        except:
-            self.meeting=None
-
-        try:
-            self.title = kwargs['title']
-        except:
-            self.title='Update'
-                        
-
+        self.title=title
+        
     def __str__(self):
         s = self.title + ' ('+ self.contributor+ ', ' + self.date + '): ' + self.link
         return s
@@ -57,82 +45,100 @@ class Study:
 ##--------------------------------------------------
 class Task:
 
-    def __init__(self,short_name,long_name,**kwargs):
+    def __init__(self,**kwargs):
         """
-        Task() class is the building block of task manager
-         - short_name: string giving shot name to be used in small tables/histogram
-         - long_name : string, giving a longer description
-         - keyword arguments can take the following:
-           * subproject: string, giving the subproject in which this task belongs to (e.g. 'l+jets')
-           * categories: array of string, giving the type of task (eg. ['optimisation', 'background understanding'])
-           * people    : array of string, giving the name of people involved
-           * prio_tasks: array of tasks, giving all tasks that should be completed before
-           * post_tasks: array of tasks, giving all tasks that need this task to be completed
+        Task() class is the building block of task manager, keyword arguments can take the following:
+           * infile     : string, input file containt the task information
+           * name       : string, giving the name of the task
+           * description: string, giving the description of the task
+           * subproject : string, giving the subproject in which this task belongs to (e.g. 'l+jets')
+           * categories : array of string, giving the type of task (eg. ['optimisation', 'background understanding'])
+           * people     : array of string, giving the name of people involved
+           * prio_tasks : array of tasks, giving all tasks that should be completed before
+           * post_tasks : array of tasks, giving all tasks that need this task to be completed
         """
-        
-        self.sname = short_name
-        self.lname = long_name
-                    
-        try:
-            self.subproject = kwargs['subproject']
-        except:
-            self.subproject=[]
-            
-        try:
-            self.cat = kwargs['categories']
-        except:
-            self.cat=[]
 
-        try:
-            self.people = kwargs['people']
-        except:
-            self.people=[]
-
-        try:
-            self.prio_tasks = kwargs['prio_tasks']
-        except:
-            self.prio_tasks=[]
-            
-        try:
-            self.post_tasks = kwargs['post_tasks']
-        except:
-            self.post_tasks=[]
-
+        self.name='name'
+        self.description='description'
+        self.subproject=''
+        self.people=[]
+        self.cat=[]
+        self.prio_tasks=[]
+        self.post_tasks=[]
         self.priority=None
         self.progress=None
+        self.comments=[]
         self.studies=[]
 
-    
-    def __copy__(self,tsk,short_name,long_name):
-        return Task(short_name,long_name,subproject=tsk.subproject, \
+        
+        if ('infile' in kwargs):
+
+            def token_info(s,t):
+                if (s[:len(t)]==t): return s[s.find(t)+len(t):].strip()
+                else: return 'dummy'
+
+            def is_token_line(l,t):
+                return l[0:len(t)]==t
+                
+            f = open(kwargs['infile'],'r')
+            for l in f.readlines():
+                l = l.strip()
+                try:
+                    if (l[0]=='#'): continue
+                except:
+                    continue
+
+                if (is_token_line(l,'.name:')):        self.name        = token_info(l,'.name:')
+                if (is_token_line(l,'.description:')): self.description = token_info(l,'.description:')
+                if (is_token_line(l,'.subproject:')):  self.subproject  = token_info(l,'.subproject:')
+                if (is_token_line(l,'.priority:')):    self.priority    = int(token_info(l,'.priority:'))
+                if (is_token_line(l,'.progress:')):    self.progress    = float(token_info(l,'.progress:'))
+                if (is_token_line(l,'.categories:')):  self.cat         = [s.strip() for s in token_info(l,'.categories:').split(',')]
+                if (is_token_line(l,'.people:')):      self.people      = [s.strip() for s in token_info(l,'.people:').split(',')]
+                if (is_token_line(l,'.comment:')):     self.add_comment( token_info(l,'.comment:') )  
+                if (is_token_line(l,'.study:')):
+                    study_data  = [s.strip() for s in token_info(l,'.study:').split(',')]
+                    self.add_study( Study(*study_data)  )  
+
+                if (is_token_line(l,'.priority:')):
+                    print(token_info(l,'.priority:'),int(token_info(l,'.priority:')))
+                    print()
+        else:
+            
+            if 'name'       in kwargs: self.name = kwargs['name']
+            if 'description'in kwargs: self.description = kwargs['description']
+            if 'subproject' in kwargs: self.subproject = kwargs['subproject']
+            if 'categories' in kwargs: self.cat = kwargs['categories']
+            if 'people'     in kwargs: self.people = kwargs['people']
+            if 'prio_tasks' in kwargs: self.prio_tasks = kwargs['prio_tasks']
+            if 'post_tasks' in kwargs: self.post_tasks = kwargs['post_tasks']
+
+        
+    def __copy__(self,tsk,name,description):
+        return Task(name=name,description=description,subproject=tsk.subproject, \
                     categories=tsk.cat,people=tsk.people,prio_tasks=tsk.prio_tasks,\
                     post_tasks=tsk.post_tasks)
 
     def __str__(self):
-        str_long       = '  Description: ' + self.lname + '\n'        
-        str_priority   = '  Priority: ' + str(self.priority) + '\n'
-        str_progress   = '  Progress: ' + str(self.progress) + '\n'
-        str_person     = '  People: '+ ', '.join(self.people) + '\n'
-        str_subproject = '  Sub-project: '+ self.subproject + '\n'
-        str_category   = '  Categories: '+ ', '.join(self.cat) + '\n'
-        str_prior      = '  Needed completed tasks: '+ ', '.join(self.prio_tasks) + '\n'
-        str_post       = '  To be completed for: '+ ', '.join(self.post_tasks) + '\n'
-        str_studies    = '  List of studies:\n'
-        for s in self.studies: str_studies += '    - ' + str(s) + '\n'
         s='\n'
-        s+='-'*(len(self.sname)+4)+'\n'
-        s+='- '+self.sname+' -\n'
-        s+='-'*(len(self.sname)+4)+'\n'
-        s+=str_long
-        if (self.subproject): s+=str_subproject
-        if (self.priority):   s+=str_priority
-        if (self.progress):   s+=str_progress
-        if (self.people):     s+=str_person
-        if (self.cat):        s+=str_category
+        s+='-'*(len(self.name)+4)+'\n'
+        s+='- '+self.name+' -\n'
+        s+='-'*(len(self.name)+4)+'\n'
+        s+='  Description: ' + self.description + '\n'
+        str_studies = '  List of studies:\n'
+        for st in self.studies: str_studies += '    - ' + str(st) + '\n'
+        str_comments = '  List of comments:\n'
+        for cm in self.comments: str_comments += '    - ' + str(cm) + '\n'
+        if (self.subproject): s+='  Sub-project: '+ self.subproject + '\n'
+        if (self.priority is not None): s+='  Priority: ' + str(self.priority) + '\n'
+        if (self.progress is not None): s+='  Progress: ' + str(self.progress) + '\n'
+        if (self.people):     s+='  People: '+ ', '.join(self.people) + '\n'
+        if (self.cat):        s+='  Categories: '+ ', '.join(self.cat) + '\n'
         if (self.studies):    s+=str_studies
+        if (self.comments):   s+=str_comments
         return s
     
-
+    
     def formatted_str(self,synthax):
         title=''
         begin_block=''
@@ -140,18 +146,18 @@ class Task:
         begin_item=''
         end_item=''
         if (synthax is 'latex'):
-            title='\\textbf{'+self.sname.replace('_','\_')+'}\n'
+            title='\\textbf{'+self.name.replace('_','\_')+'}\n'
             begin_block='\\begin{itemize}\n'
             end_block='\\end{itemize}\n'
             begin_item='\\item '
             end_item='\n'
         elif (synthax is 'md'):
-            title='**'+self.sname+'**\n'
+            title='**'+self.name+'**\n'
             begin_item=' * '
             end_item='\n'
         elif (synthax is 'html'):
-            title='<b>'+self.sname+'</b>\n'
-            begin_block='<ul>'
+            title='<b>'+self.name+'</b>\n'
+            begin_block='<ul>\n'
             end_block='</ul>\n'
             begin_item='<li>'
             end_item='</li>\n'
@@ -163,22 +169,27 @@ class Task:
         s=''
         s+=title
         s+=begin_block
-        s+=begin_item + 'Description: '+ self.lname   + end_item
-        if (self.subproject): s+=begin_item + 'Sub-project: '+ self.subproject   + end_item
-        if (self.priority):   s+=begin_item + 'Priority: ' + str(self.priority)  + end_item
-        if (self.progress):   s+=begin_item + 'Progress: ' + str(self.progress)  + end_item
-        if (self.people):     s+=begin_item + 'People: '+ ', '.join(self.people)  + end_item
-        if (self.cat):        s+=begin_item + 'Categories: '+ ', '.join(self.cat) + end_item
+        s+=begin_item + 'Description: '+ self.description  + end_item
+        if (self.subproject):           s+=begin_item + 'Sub-project: '+ self.subproject   + end_item
+        if (self.priority is not None): s+=begin_item + 'Priority: ' + str(self.priority)  + end_item
+        if (self.progress is not None): s+=begin_item + 'Progress: ' + str(self.progress)  + end_item
+        if (self.people):               s+=begin_item + 'People: '+ ', '.join(self.people)  + end_item
+        if (self.cat):                  s+=begin_item + 'Categories: '+ ', '.join(self.cat) + end_item
         if (self.studies):
             s+=begin_item + 'List of studies:' + end_item
             s+=begin_block
             for stu in self.studies: s+= '  ' + begin_item + stu.formatted_str(synthax) + end_item
             s+=end_block
         s+=end_block
+        if (self.comments):
+            s+=begin_item + 'List of comments:' + end_item
+            s+=begin_block
+            for cm in self.comments: s+= '  ' + begin_item + cm + end_item
+            s+=end_block
         return s
     
-    def copy(self,short_name,long_name):
-        return self.__copy__(self,short_name,long_name)
+    def copy(self,name,description):
+        return self.__copy__(self,name,description)
     
     def set_priority(self,p):
         self.priority=p
@@ -194,6 +205,9 @@ class Task:
 
     def set_people(self,persons):
         self.people=persons
+
+    def add_comment(self, item):
+        self.comments.append(item)
         
     def add_categories(self,cats):
         for c in cats:
@@ -232,14 +246,41 @@ class Project:
 
     def set_tasks(self,tasks):
         self.tasks = tasks
-        
+
+    def load_tasks_file(self,filename):
+        f = open(filename,'r')
+        i_task=0
+        for l in f.readlines():
+
+            # Clean the line and remove commented line '#'
+            l = l.strip()
+            try:
+                if l[0]=='#': continue
+            except:
+                continue
+
+            # Keep track of when the task is changing
+            local_task_index=i_task
+            tmp_file = open('tmp.task','w+')
+            if (l=='+tsk'):
+                if (i_task>0):
+                    tmp_file.close()
+                    self.add_task( Task(infile='tmp.task') )
+                i_task+=1
+              
+            if (local_task_index==i_task):
+                tmp_file.write(l)
+                for l2 in tmp_file: print(l2)
+
+    
     def add_task(self,task):
         self.tasks.append(task)
 
     def add_tasks(self,tasks):
         for t in tasks: self.tasks.append(t)
 
-    def get_tasks(self,subproject)
+    def get_tasks(self,subproject):
+        return []
         
     def subprojects(self):
         subproj = [t.subproject for t in self.tasks]
@@ -250,5 +291,5 @@ class Project:
         return list(set(contribs))
 
     def dataframe(self):
-        import pandas as pd
+        import pandas as pd        
         return pd.DataFrame()
--- eg-project.py
+++ eg-project.py
@@ -1,47 +1,24 @@
-import analysis_management as pm
+import analysis_management as am
 
 # task 1
-lep_kinem_ljets = pm.Task('lep_kinem_ljets','Optimization of the lepton kinematics in the l+jets/os2L channel')
-lep_kinem_ljets.set_subproject('l+jets')
-lep_kinem_ljets.add_categories(['optimizations'])
-lep_kinem_ljets.add_people(['Matt K.','Simon'])
-lep_kinem_ljets.set_priority(1)
-lep_kinem_ljets.set_progress(0)
-
-# task 2
-lep_kinem_ss2L = pm.Task('lep_kinem_ss2L','Optimization of the lepton kinematics in the ss2L/3L channel')
-lep_kinem_ss2L.set_subproject('ss2L/3L')
-lep_kinem_ss2L.set_categories(['optimizations'])
-lep_kinem_ss2L.set_people(['Shuyang'])
-lep_kinem_ss2L.set_priority(1)
-lep_kinem_ss2L.set_progress(0)
-lep_kinem_ss2L.add_study( pm.Study('11-04-2018','Zhi','https://indico.cern.ch/event/696609/contributions/2943411/attachments/1625965/2589626/OverlapRemovalStudy.pdf')  )
-
-# task 3
-btagging_ss2L = pm.Task('btagging_ss2L','Optimization of the b-tagging working point for the ss2L/3L channel')
-btagging_ss2L.set_subproject('ss2L/3L')
-btagging_ss2L.set_categories(['optimizations'])
-btagging_ss2L.set_people(['Romain'])
-btagging_ss2L.set_priority(1)
-btagging_ss2L.set_progress(0.5)
-btagging_ss2L.add_study( pm.Study('03-04-2018','Romain','https://indico.cern.ch/event/696609/contributions/2943410/attachments/1625903/2589232/Btagging.pdf') )
-
-# task 4
-NP_naming = pm.Task('NP_naming','Design the naming convention of nuisance paramter for the combination')
-NP_naming.set_subproject('combination')
-NP_naming.set_people(['Robert Deniro'])
-
+#task1 = am.Task(name='task1',description='Optimization of the lepton kinematics in the l+jets/os2L channel')
+#task1.set_subproject('l+jets')
+#task1.add_categories(['optimizations'])
+#task1.add_people(['Matt K.','Simon'])
+#task1.set_priority(1)
+#task1.set_progress(0)
 
 # A study
-contrib_test  = pm.Study('12-03-2017','Romain Madar','http://romain-madar.com')
-lep_kinem_ljets.add_study(contrib_test)
+#contrib_test=am.Study('12-03-2017','Romain Madar','http://romain-madar.com')
+#task1.add_study(contrib_test)
 
-# Array of tasks
-lepton_tasks = [lep_kinem_ljets,lep_kinem_ss2L,btagging_ss2L,NP_naming]
-    
+task2 = am.Task(infile='one_task.task')
+#print(task2)
+#print(task2.formatted_str('html'))
 
 # Project
-my_project = pm.Project('4topSM')
-my_project.add_tasks(lepton_tasks)
+my_project = am.Project('4topSM')
+#my_project.add_tasks(lepton_tasks)
+my_project.load_tasks_file('lepton.tasks')
 print(my_project.subprojects())
-print(my_project.contributors())
+#print(my_project.contributors())
