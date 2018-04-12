de89d99a2aa9bd689d3843e27729774f4cc6f739
Romain Madar <romain.madar@cern.ch>
2018-04-12 15:43:57 +0200

    analysis_management.py

--- eg-project.py
+++ eg-project.py
@@ -1,24 +1,36 @@
 import analysis_management as am
 
 # task 1
-#task1 = am.Task(name='task1',description='Optimization of the lepton kinematics in the l+jets/os2L channel')
-#task1.set_subproject('l+jets')
-#task1.add_categories(['optimizations'])
-#task1.add_people(['Matt K.','Simon'])
-#task1.set_priority(1)
-#task1.set_progress(0)
+task1 = am.Task(name='task1',description='Test a standalone task')
+task1.set_subproject('combination')
+task1.add_categories(['systematics'])
+task1.add_people(['Jean-Claude','Pierre'])
+task1.set_priority(1)
+task1.set_progress(0)
 
 # A study
-#contrib_test=am.Study('12-03-2017','Romain Madar','http://romain-madar.com')
-#task1.add_study(contrib_test)
+contrib_test=am.Study('12-03-2017','Jacques','http://google.com')
+task1.add_study(contrib_test)
 
+# Read task from as single-task file
 task2 = am.Task(infile='one_task.task')
-#print(task2)
-#print(task2.formatted_str('html'))
 
 # Project
 my_project = am.Project('4topSM')
-#my_project.add_tasks(lepton_tasks)
-my_project.load_tasks_file('lepton.tasks')
-print(my_project.subprojects())
-#print(my_project.contributors())
+
+# Load task file project
+my_project.load_tasks_file('lepton.task')
+
+# Add some taks on the fly
+my_project.add_tasks([task1,task2])
+
+
+
+print(my_project.get_subprojects())
+print(my_project.get_categories())
+
+for t in my_project.get_tasks():
+    print(t.name,t.progress)
+
+#for t in my_project.get_tasks():
+#    print(t.formatted_str('html'))
